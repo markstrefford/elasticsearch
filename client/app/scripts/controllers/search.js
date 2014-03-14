d3ff330ccc6bbc9fe4ea0e6cce0479090e642c3b
Mark Strefford <markstrefford@me.com>
2014-03-14 08:21:06 +0000

    Working search, maps and example search json

--- client/app/scripts/controllers/search.js
+++ client/app/scripts/controllers/search.js
@@ -2,63 +2,77 @@
 
 angular.module('search', ['leaflet-directive', 'esService'])
     .controller('SearchCtrl', ['$scope', 'es', function ($scope, es) {
+
+        var m = {},
+            h = {};
+
         console.log("Setting default lat/long and zoom...");
+        // TODO - Make this auto-centre later...
         angular.extend($scope, {
             center: {
-                lat: 53.35,
-                lng: -2.3683596,
-                zoom: 9
+                lat: 53.45,
+                lng: -2.48,
+                zoom: 10
             },
             markers: {}
         })
-        es.ping({
-            requestTimeout: 1000,
-            hello: "elasticsearch!"
-        }, function (error) {
-            if (error) {
-                console.error('elasticsearch cluster is down!');
-            } else {
-                console.log('All is well');
-            }
-        });
+
+        // Setting scope for hotels later
+        angular.extend($scope, { hotels: {}});
 
         // Now do a search
-        var searchReq = { "fields": [ "id", "name", "geo"],
+        // TODO - Make this dynamic!!
+
+        var searchReq = {   "fields": [ "id", "name", "geo", "description", "starrating", "custrating"],
+            "from" : 0, "size" : 1000,
             "query": {
-                "filtered": {
-                    "query": {
-                        "match_all": {}
-                    },
-                    "filter": {
-                        "geo_distance": {
-                            "distance": "2km",
-                            "geo": [-2.9897, 53.39879]
-                        }
+                "matchAll": {}
+            },
+            "sort": [
+                {
+                    "_geo_distance": {
+                        "geo": "-2.9897, 53.39879",
+                        "unit": "km"
                     }
                 }
-            }};
-        var m = {};
+            ]
+        };
+
+        /*
+         var searchReq= { "fields": [ "id", "name", "geo", "description"],
+         "query": {
+         "match_all": {}
+         }
+         }
+         */
+
+        // Use promises here based on http://brianoneill.blogspot.co.uk/2014/01/elasticsearch-from-angularjs-fun-w.html
         es.search({
             host: 'localhost:9200/hotels/hotel',
             body: searchReq
         }).then(function (body) {
                 console.log("Hits: " + JSON.stringify(body.hits.hits));
+                // Iterate through results and update $scope
                 for (var i in body.hits.hits) {
                     var hotel = body.hits.hits[i];
-                    var hotelId = hotel._id;
-                    var hotelName = hotel.fields.name;
-                    var hotelLoc = hotel.fields.geo;
-                    console.log("Id:" + hotelId + ",Name:" + hotelName + ",Geo:" + hotelLoc);
-                    //$scope.markers[hotel._id.geo]={type: "point", "location": [hotelLoc]};
-                    //$scope.markers[hotel._id.message]= hotelName;
-                    m[hotelId] = {
-                        "location": {"type": "point", "location": hotelLoc},
-                        "message": hotelName
+                    console.log("Processing " + JSON.stringify(hotel));
+                    m[hotel._id] = {
+                        "lat": hotel.fields.geo.split(",")[0],
+                        "lng": hotel.fields.geo.split(",")[1],
+                        "message": hotel.fields.name,
+                        "draggable": false
                     };
+                    h[hotel._id] = {
+                        "name": hotel.fields.name,
+                        "description": hotel.fields.description,
+                        "starrating" : hotel.fields.starrating,
+                        "custrating" : hotel.fields.custrating
+                    }
                 }
                 //console.log(JSON.stringify($scope.markers));
                 console.log(JSON.stringify(m));
-                $scope.markers=m;
+                $scope.markers = m;
+                $scope.hotels = h;
             }, function (error) {
                 console.trace(error.message);
             });
@@ -66,11 +80,3 @@ angular.module('search', ['leaflet-directive', 'esService'])
 ;
 
 
-//var markers = {
-//    school: {
-//        lat: $scope.school.loc.coordinates[1],
-//        lng: $scope.school.loc.coordinates[0],
-//        draggable: false,
-//        message: $scope.school.name,
-//        focus: true
-//    }
