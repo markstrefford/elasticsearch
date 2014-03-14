fb35b2c16d3a0a13dfe92c9677184a69d34ea615
Mark Strefford <markstrefford@me.com>
2014-03-14 04:44:56 +0000

    Initial leaflet map drawn

--- client/app/scripts/controllers/search.js
+++ client/app/scripts/controllers/search.js
@@ -1,10 +1,30 @@
 'use strict';
 
-angular.module('search', ['elasticsearch'])
-  .controller('SearchCtrl', ['$scope', 'esService', function ($scope, esService) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate for Search and ElasticSearch',
-      'AngularJS',
-      'Karma'
-    ];
+angular.module('search', ['leaflet-directive', 'elasticsearch'])
+  .controller('SearchCtrl', ['$scope', 'esService', function ($scope) {
+        console.log("Setting default lat/long and zoom...");
+        angular.extend($scope, {
+            center: {
+                lat: 53.35,
+                lng: -2.3683596,
+                zoom: 9
+            },
+            markers: {}
+        })
   }]);
+
+
+
+
+//app.controller("CenterController", [ '$scope', function($scope) {
+//    angular.extend($scope, {
+//        center: {
+//            lat: 40.095,
+//            lng: -3.823,
+//            zoom: 4
+//        },
+//        defaults: {
+//            scrollWheelZoom: false
+//        }
+//    });
+//}]);
