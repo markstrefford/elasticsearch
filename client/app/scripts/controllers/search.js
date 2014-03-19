28f2c55ce1543a95d70bee607faa0ee44c937bd6
Mark Strefford <markstrefford@me.com>
2014-03-19 19:51:50 +0000

    Working map redraw!!

--- client/app/scripts/controllers/search.js
+++ client/app/scripts/controllers/search.js
@@ -3,37 +3,48 @@
 angular.module('searchModule', ['leaflet-directive', 'esService'])
     .controller('SearchCtrl', ['$scope', 'es', function ($scope, es) {
 
-        console.log("Setting default lat/long and zoom...");
-        // TODO - Make this auto-centre later...
         var search={};
-
         angular.extend($scope, {center: {
                     lat: 53.45,
                     lng: -2.48,
                     zoom: 10
                 }});
-        $scope.markers= {};
-        $scope.hotels = {};
+        angular.extend($scope, {markers: {}});
+        angular.extend($scope, {hotels: {}});
 
         // Now do the default search
         // TODO - Make this dynamic!!
-        search.request = {   "fields": [ "id", "name", "geo", "description", "starrating", "custrating"],
-            "from": 0, "size": 5,
+        //search.request = {   "fields": [ "id", "name", "geo", "description", "starrating", "custrating"],
+        //    "from": 0, "size": 5,
+        //    "query": {
+        //        "matchAll": {}
+        //    },
+        //    "sort": [
+        //        {
+        //            "_geo_distance": {
+        //                "geo": "-2.9897, 53.39879",
+        //                "unit": "km"
+        //            }
+        //        }
+        //    ]
+        //};
+
+        search.request = { "fields": [ "id", "name", "geo", "description", "starrating", "custrating"],
             "query": {
-                "matchAll": {}
-            },
-            "sort": [
-                {
-                    "_geo_distance": {
-                        "geo": "-2.9897, 53.39879",
-                        "unit": "km"
-                    }
+                "term": {
+                    "description": "spa"
                 }
-            ]
-        };
+            }}
+
+
+        // Called from search.html "Search" button
+        $scope.refineSearch = function (request) {
+            newSearch(request);
+
+            //$scope.$apply();
+        }
 
         var newSearch = function (searchReq) {
-            console.log("Starting search()...");
             console.log("search(): " + JSON.stringify(searchReq));
             var m = {},
                 h = {};
@@ -44,11 +55,8 @@ angular.module('searchModule', ['leaflet-directive', 'esService'])
             }).then(function (body) {
                     // Iterate through results and update $scope
                     var searchRes = body.hits.hits;
-                    console.log("SearchRes: " + JSON.stringify(searchRes));
                     for (var i in searchRes) {
-                        console.log("searchRes["+i+"]:" +  JSON.stringify(searchRes));
                         var hotel = searchRes[i];
-                        //console.log("Processing " + JSON.stringify(hotel));
                         m[hotel._id] = {
                             "lat": hotel.fields.geo.split(",")[0],
                             "lng": hotel.fields.geo.split(",")[1],
@@ -63,25 +71,17 @@ angular.module('searchModule', ['leaflet-directive', 'esService'])
 
                         }
                     }
-                    //console.log(JSON.stringify($scope.markers));
-                    //console.log(JSON.stringify(m));
-                    console.log("New markers[]:" + JSON.stringify(m));
-                    $scope.markers = m;
-                    $scope.hotels = h;
+                    $scope.hotels=h;
+                    $scope.markers=m;
+                    $scope.apply;
                 }, function (error) {
                     console.trace(error.message);
                 });
+            //return {markers: m, hotels: h};
         }
 
         // Now render the map with the default search
         newSearch(search.request);
-
-
-        // Called from search.html "Search" button
-        $scope.refineSearch = function (request) {
-            console.log("New search : " + request);
-            newSearch(request);
-        }
 
     }])
 ;
